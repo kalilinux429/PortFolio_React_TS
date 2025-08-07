63d544230653750898e24d344bd89d8afca5e74a
kalilinux429 <suchitkapale@gmail.com>
2025-08-07 11:28:03 +0530

    inital commit

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,7 +13,7 @@ export const projects = [
     title: "Code Compiler",
     description: "A collaborative task management application with real-time updates",
     technologies: ["React", "monoCo Editor", "Tailwind CSS"],
-    imageUrl: "https://unsplash.com/photos/macbook-pro-on-black-wooden-table-PNbDkQ2DDgM",
+    imageUrl: "https://private-user-images.githubusercontent.com/77877663/475402915-c5751e0f-6f71-4627-977a-34f4fd1110dd.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTQ1NDU5NTcsIm5iZiI6MTc1NDU0NTY1NywicGF0aCI6Ii83Nzg3NzY2My80NzU0MDI5MTUtYzU3NTFlMGYtNmY3MS00NjI3LTk3N2EtMzRmNGZkMTExMGRkLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA4MDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwODA3VDA1NDczN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQwODIyNTFmYzVkMTlmYmQxMWU3YmEwYzIwMzhlZWRlN2M2ZTQ2Yjg1MjBkMGM3NDViMGU4ZDJiZjgyOGY3OTUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.tLDhLFN2MgrHRUvlm2fctTNFhZS2PNVRMWNsHcVbfV0",
     githubUrl: "#",
     liveUrl: "https://react-code-editor-one.vercel.app/"
 
@@ -26,5 +26,16 @@ export const projects = [
     imageUrl: "https://images.unsplash.com/photo-1592210454359-9043f067919b?auto=format&fit=crop&q=80",
     githubUrl: "#",
     liveUrl: "https://map-lines-assessment.vercel.app/"
-  }
+  },
+
+
+   {
+    id: 1,
+    title: "Code-Snippet-Saver",
+    description: "Code Snippet Saver for Developers",
+    technologies: ["React", "Tailwind"],
+    imageUrl: "https://private-user-images.githubusercontent.com/77877663/475399329-be8bc2c9-7384-4ac3-a161-74a430e35c66.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTQ1NDUzOTAsIm5iZiI6MTc1NDU0NTA5MCwicGF0aCI6Ii83Nzg3NzY2My80NzUzOTkzMjktYmU4YmMyYzktNzM4NC00YWMzLWExNjEtNzRhNDMwZTM1YzY2LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA4MDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwODA3VDA1MzgxMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWYwNGJhZWE5MWQyYzRkZWU0ZWM3MDYzZjBjZjNkNWFjMTNiMGNmNjdkNmVmYzFkMDE3YTliY2EzNTRhMjliOTAmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.Uf-SaoLtw-ACrfMa8VImX07vPBl74TefRtnWpuW8Km4",
+    githubUrl: "",
+    liveUrl: "https://code-snippet-saver-six.vercel.app"
+  },
 ];
